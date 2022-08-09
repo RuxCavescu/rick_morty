eb48e5e2aa80365b1b59d665c776f133dd2ea5c3
RuxCavescu <rux.cavescu@gmail.com>
2022-08-09 16:48:30 +0200

    final version

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { InfiniteQueryObserver, useQuery } from 'react-query';
+import Character from './Character';
 
 function Characters() {
   //   const [characters, setCharacters] = useState([]);
 
-  const fetchCharacters = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character');
+  const [page, setPage] = useState(1);
+
+  const fetchCharacters = async ({ queryKey }) => {
+    // query key is what we pass via the useQuery, in this case ['characters', page]
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
     // const data = await response.json();
     // setCharacters(data.results);
 
@@ -13,7 +19,10 @@ function Characters() {
   };
 
   // react-query needs a key to understand which query it has fetched 'characters'
-  const { data, status } = useQuery('characters', fetchCharacters);
+  const { data, status, isPreviewsData, isLoading, isError } = useQuery(
+    ['characters', page],
+    fetchCharacters
+  );
   // and a function which fetches and returns the data fetchCharacters
 
   // the query returns the data and the status of the fetch (loading or error or success)
@@ -22,11 +31,13 @@ function Characters() {
   //     fetchCharacters();
   //   }, []);
 
-  if (status === 'loading') {
+  console.log(data);
+
+  if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (status === 'error') {
+  if (isError) {
     return <div>Error!</div>;
   }
 
@@ -36,9 +47,24 @@ function Characters() {
         <div key={character.id}>{character.name}</div>
       ))} */}
 
-      {data.results.map((character) => (
+      {/* {data.results.map((character) => (
         <div key={character.id}>{character.name}</div>
+      ))} */}
+
+      {data.results?.map((character) => (
+        <Character key={character.id} character={character} />
       ))}
+      <div>
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          Prev
+        </button>
+        <button
+          disabled={isPreviewsData && data.info.next == null}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
